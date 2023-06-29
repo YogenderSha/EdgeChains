e4ff776139dc11ef9331131eac585728ba40944b
Yogender Sharma <sharmayogender572@gmail.com>
2023-06-29 18:50:49 +0530

    Update EdgeChainServiceRunner.java
    1)Removed the Unused import java.io.FileInputStream
    2) Setting Environment Variables in the Configuration instead of setting system variables directly in the code.

--- edgechain-app/src/main/java/com/edgechain/service/EdgeChainServiceRunner.java
+++ edgechain-app/src/main/java/com/edgechain/service/EdgeChainServiceRunner.java
@@ -3,7 +3,6 @@ package com.edgechain.service;
 import com.edgechain.lib.configuration.EdgeChainAutoConfiguration;
 import com.edgechain.service.constants.ServiceConstants;
 import java.io.File;
-import java.io.FileInputStream;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,17 +18,7 @@ public class EdgeChainServiceRunner {
 
   public static void main(String[] args) throws Exception {
 
-    System.setProperty("server.port", "8002");
-
-    System.setProperty("spring.data.redis.host", "");
-    System.setProperty("spring.data.redis.port", "6379");
-    System.setProperty("spring.data.redis.username", "default");
-    System.setProperty("spring.data.redis.password", "");
-    System.setProperty("spring.data.redis.connect-timeout", "120000");
-    System.setProperty("spring.redis.ttl", "3600");
-    System.setProperty("jsonnet.target.location", "");
-    System.setProperty("openai.api.key", "");
-    System.setProperty("doc2vec.filepath", "R:\\doc_vector.bin");
+    
 
     readDoc2Vec();
 
